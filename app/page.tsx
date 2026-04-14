9cb3c14f8971994e40fb12d477e3b2846b222297
Bryant Oliver <bryantcleve@gmail.com>
2026-04-14 12:24:02 -0700

    Added testing architecture

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-800 text-white flex flex-col items-center justify-center">
-      <h1 className="text-5xl font-bold">Tiki Art</h1>
+      <h1 className="text-5xl font-bold">FirelordArts</h1>
+      <Image
+        src="/images/img_9922.jpg"
+        alt="Aidan"
+        width={2160 / 4}
+        height={2799 / 4}
+      />
       <p className="mt-4 text-lg">Original handcrafted pieces</p>
     </main>
   );
